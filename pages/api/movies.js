9e05d7eb61395ef749610ff1edcb90afc8f517d9
Clément <japanclems287@gmail.com>
2024-03-19 13:49:53 +0100

    feat: :memo: add swagger oas3

--- pages/api/movies.js
+++ pages/api/movies.js
@@ -1,5 +1,14 @@
 import clientPromise from "../../lib/mongodb";
 
+/**
+* @swagger
+* /api/movies:
+*   get:
+*     description: Returns all movies
+*     responses:
+*       200:
+*         description: You got your movies list
+*/
 export default async function handler(req, res) {
     const client = await clientPromise;
     const db = client.db("sample_mflix");
